7fc65f38670568b881e55da21f817899f35c37d5
Jean <jlabedo@gmail.com>
2016-04-14 12:35:57 +0200

    Linting

--- src/ServerAction.js
+++ src/ServerAction.js
@@ -1,45 +1,45 @@
 export default class ServerAction {
-  constructor(opts) {
-    this.name = opts.name;
-    this.onLoad = opts.onLoad;
-    this.onSuccess = opts.onSuccess;
-    this.onFail = opts.onFail;
-    this.stateAccessor = opts.stateAccessor;
-    this.serve = opts.serve;
+  constructor (opts) {
+    this.name = opts.name
+    this.onLoad = opts.onLoad
+    this.onSuccess = opts.onSuccess
+    this.onFail = opts.onFail
+    this.stateAccessor = opts.stateAccessor
+    this.serve = opts.serve
   }
 
-  generateReducers() {
-    let reducers = {};
+  generateReducers () {
+    let reducers = {}
     reducers[this.name + '_REQUEST'] = {
       reducer: this.onLoad,
-      stateAccessor : this.stateAccessor
-    };
+      stateAccessor: this.stateAccessor
+    }
     reducers[this.name + '_SUCCESS'] = {
       reducer: this.onSuccess,
-      stateAccessor : this.stateAccessor
-    };
+      stateAccessor: this.stateAccessor
+    }
     reducers[this.name + '_FAILURE'] = {
       reducer: this.onFail,
-      stateAccessor : this.stateAccessor
-    };
-    return reducers;
+      stateAccessor: this.stateAccessor
+    }
+    return reducers
   }
 
   dispatch = (data) => {
-    if(!this._dispatch) {
-      console.error('No dispatch method has been registered');
+    if (!this._dispatch) {
+      console.error('No dispatch method has been registered')
     }
-    const _d = this._dispatch;
+    const _d = this._dispatch
     _d({
       type: this.name + '_REQUEST',
       data
-    });
+    })
     this.serve(data).then(
       (result) => _d({type: this.name + '_SUCCESS', result}),
       (error) => _d({type: this.name + '_FAILURE', error})
-    ).catch((error)=> {
-      console.error('ServerAction ERROR:', error);
-      _d({type: this.name + '_FAILURE', error});
-    });
+    ).catch((error) => {
+      console.error('ServerAction ERROR:', error)
+      _d({type: this.name + '_FAILURE', error})
+    })
   }
 }
--- src/StateAccessor.js
+++ src/StateAccessor.js
@@ -1,23 +1,23 @@
 export default class StateAccessor {
-  constructor(key, initialValue) {
-    this.key = key;
-    this.initialValue = initialValue;
+  constructor (key, initialValue) {
+    this.key = key
+    this.initialValue = initialValue
   }
 
-  getState(state) {
-      return state[this.key] || this.initialValue;
+  getState (state) {
+    return state[this.key] || this.initialValue
   }
 
-  updateState(state, newValue) {
+  updateState (state, newValue) {
     if (state[this.key] !== newValue) {
       return {...state, [this.key]: newValue}
     } else {
-      return state;
+      return state
     }
   }
 
-  init(state) {
-    this.initialized = true;
-    return this.updateState(state, this.initialValue);
+  init (state) {
+    this.initialized = true
+    return this.updateState(state, this.initialValue)
   }
 }
